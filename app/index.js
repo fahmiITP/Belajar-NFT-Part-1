a557cea2cbebbc5556f93b65e2495ff8f84db962
Fahmi ITP <fahmi@itprovent.com>
2021-03-15 18:54:11 +0700

    Changes Port

--- app/index.js
+++ app/index.js
@@ -57,6 +57,6 @@ app.use((err, req, res, next) => {
     return;
 });
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+app.listen((process.env.PORT || 3000), () => {
+    console.log("Server running on port " + (process.env.PORT || "3000"));
 });
