99c37f210bc483e821c15a9f1a8e8b5b2fe261d1
Fahmi ITP <fahmi@itprovent.com>
2021-03-18 15:19:28 +0700

    Added Get All Contracts Query Even if The User isn't the owner.

--- db/contracts/contracts.js
+++ db/contracts/contracts.js
@@ -1,46 +1,53 @@
-const db = require('../db');
-const helper = require('../helper');
-const config = require('../config');
+const db = require("../db");
+const helper = require("../helper");
+const config = require("../config");
 
 /// Get contract data based on contract address
 async function getContract(contract) {
-    const rows = await db.query(
-        `SELECT id, contract_owner, contract_address, contract_abi FROM contracts WHERE contract_address = "${contract.contract_address}"`,
-    );
+  const rows = await db.query(
+    `SELECT id, contract_owner, contract_address, contract_abi FROM contracts WHERE contract_address = "${contract.contract_address}"`
+  );
 
-    return { rows }
+  return { rows };
 }
 
-/// Get user contract data based
+/// Get user contract data based on user's address
 async function getUserContracts(contract) {
-    const rows = await db.query(
-        `SELECT id, contract_owner, contract_address, contract_abi FROM contracts WHERE contract_owner LIKE "${contract.owner_address}"`,
-    );
-    return { rows }
+  const rows = await db.query(
+    `SELECT id, contract_owner, contract_address, contract_abi FROM contracts WHERE contract_owner LIKE "${contract.owner_address}"`
+  );
+  return { rows };
+}
+
+/// Get all user contract even if the owner isn't that user. (Probably has a token that was transfered there).
+async function getAllUserContract(address) {
+  const rows = await db.query(
+    `SELECT DISTINCT contracts.contract_owner, contracts.contract_address FROM contracts 
+    LEFT JOIN token ON contracts.contract_address = token.contract_address 
+    WHERE token.token_owner = "${address.owner_address}" OR contracts.contract_owner = "${address.owner_address}"`
+  );
+  return { rows };
 }
 
 /// Create a new contract to the DB
 async function create(contract) {
-    const result = await db.query(
-        `INSERT INTO contracts (contract_owner, contract_address, contract_abi) VALUES (?,?,?)`,
-        [
-            contract.contract_owner,
-            contract.contract_address,
-            contract.contract_abi
-        ]
-    );
+  const result = await db.query(
+    `INSERT INTO contracts (contract_owner, contract_address, contract_abi) VALUES (?,?,?)`,
+    [contract.contract_owner, contract.contract_address, contract.contract_abi]
+  );
 
-    let message = 'Error in creating contract';
+  let message = "Error in creating contract";
 
-    if (result.affectedRows) {
-        message = 'Contract created successfully';
-    }
+  if (result.affectedRows) {
+    message = "Contract created successfully";
+  }
 
-    return { message };
+  return { message };
 }
 
 module.exports = {
-    getContract,
-    getUserContracts,
-    create
-}
+  getContract,
+  getUserContracts,
+  getAllUserContract,
+  create,
+};
