766a58afb9a2483d16a6c148d4874972064c59cf
Fahmi ITP <67779238+fahmiITP@users.noreply.github.com>
2021-03-18 15:22:32 +0700

    Added Get All Contracts Query Even if The User isn't the owner. (#2)

--- app/routes/contracts.js
+++ app/routes/contracts.js
@@ -1,35 +1,48 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const contract = require('../../db/contracts/contracts');
+const contract = require("../../db/contracts/contracts");
 
 /* Get One Contract. */
-router.post('/', async function (req, res, next) {
-    try {
-        res.json(await contract.getContract(req.body));
-    } catch (err) {
-        console.error(`Error while getting contract `, err.message);
-        next(err);
-    }
+router.post("/", async function (req, res, next) {
+  try {
+    res.json(await contract.getContract(req.body));
+  } catch (err) {
+    console.error(`Error while getting contract `, err.message);
+    next(err);
+  }
 });
 
 /* Get User Contract. */
-router.post('/userContracts', async function (req, res, next) {
-    try {
-        res.json(await contract.getUserContracts(req.body));
-    } catch (err) {
-        console.error(`Error while getting contract `, err.message);
-        next(err);
-    }
+router.post("/userContracts", async function (req, res, next) {
+  try {
+    res.json(await contract.getUserContracts(req.body));
+  } catch (err) {
+    console.error(`Error while getting contract `, err.message);
+    next(err);
+  }
+});
+
+/* Get All User Contracts (Even if the user isn't the owner) 
+   body: {owner_address: 0x00000}
+*/
+
+router.post("/getAllContract", async function (req, res, next) {
+  try {
+    res.json(await contract.getAllUserContract(req.body));
+  } catch (err) {
+    console.error(`Error while getting contract `, err.message);
+    next(err);
+  }
 });
 
 /* POST Contract */
-router.post('/add', async function (req, res, next) {
-    try {
-        res.json(await contract.create(req.body));
-    } catch (err) {
-        console.error(`Error while creating contract`, err.message);
-        next(err);
-    }
+router.post("/add", async function (req, res, next) {
+  try {
+    res.json(await contract.create(req.body));
+  } catch (err) {
+    console.error(`Error while creating contract`, err.message);
+    next(err);
+  }
 });
 
 module.exports = router;
