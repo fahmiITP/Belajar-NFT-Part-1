acff7048c0cf7962e1e022b3b878c545f520120b
Fahmi ITP <fahmi@itprovent.com>
2021-03-24 15:20:41 +0700

    Fixed Some Conflicting Items

--- contracts/UserContract.sol
+++ contracts/UserContract.sol
@@ -40,13 +40,4 @@ contract UserContract is NFTokenMetadata, Ownable, Store {
     function burn(uint256 _tokenId) external onlyOwner {
         super._burn(_tokenId);
     }
-
-    /**
-     * @dev Buy an NFT.
-     * @param _tokenId of the NFT to be bought by the msg.sender.
-     */
-
-    function buy(address buyer, uint256 _tokenId) external payable {
-        super._buy(buyer, _tokenId);
-    }
 }
