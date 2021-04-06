60bfefb0962359f7b394a4c58608d49a51ec71db
Fahmi ITP <fahmi@itprovent.com>
2021-04-06 14:50:22 +0700

    Fixed Contract Bytecodes Not Found

--- services/user_contract/getUserContractBytecode.js
+++ services/user_contract/getUserContractBytecode.js
@@ -1,13 +1,17 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 module.exports = function () {
-    /// Search for User Contract
-    const UserCompiledContractPath = path.resolve(__dirname, '../build/contracts', 'UserContract.json');
-    /// Get the data
-    let rawdata = fs.readFileSync(UserCompiledContractPath);
-    /// Parse JSON
-    let contract = JSON.parse(rawdata);
-    /// Return only bytecode
-    return contract.bytecode;
-}
+  /// Search for User Contract
+  const UserCompiledContractPath = path.resolve(
+    __dirname,
+    "../../build/contracts",
+    "UserContract.json"
+  );
+  /// Get the data
+  let rawdata = fs.readFileSync(UserCompiledContractPath);
+  /// Parse JSON
+  let contract = JSON.parse(rawdata);
+  /// Return only bytecode
+  return contract.bytecode;
+};
